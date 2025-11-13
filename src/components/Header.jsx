c41c2a6b3eb597ccee270b1a7504160606629079
swiftybo <leungjordan28@gmail.com>
2025-11-13 22:18:24 +0000

    Added img and h1 elements for lhs section of the header. Styling and google font added for these elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,12 @@ export default function Header() {
   return (
     <header className="header">
       <section className="header__section section-left">
-        <img src={shufflerLogo} alt="Shuffler App Logo" />
-        <h1>Shuffler</h1>
+        <img
+          src={shufflerLogo}
+          alt="Shuffler App Logo"
+          className="header__img"
+        />
+        <h1 className="header__title oswald">shuffler</h1>
       </section>
       <section className="header__section section-right">
         <div></div>
