12182abd9644929310dd2f40637e32aaa4e16f8f
swiftybo <leungjordan28@gmail.com>
2025-11-13 21:48:59 +0000

    LHS and RHS sections and associated classes created within the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,13 @@ import shufflerLogo from "../assets/shuffler-logo.png";
 
 export default function Header() {
   return (
-    <header>
-      <section>
+    <header className="header">
+      <section className="header__section section-left">
         <img src={shufflerLogo} alt="Shuffler App Logo" />
         <h1>Shuffler</h1>
+      </section>
+      <section className="header__section section-right">
+        <div></div>
       </section>
     </header>
   );
