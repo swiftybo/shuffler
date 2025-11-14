51e3843daf5383e10137f7f7fb6b469b86fe68ae
swiftybo <leungjordan28@gmail.com>
2025-11-14 09:37:07 +0000

    New content section created below the header to contain the menu component. Into section also created with styling completed.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ function App() {
   return (
     <>
       <Header />
-      <Menu />
+      <section className="content">
+        <div className="intro josefin-sans">
+          <h1>What should we do today?</h1>
+          <p className="intro__para">
+            Let the Shuffler decide! Choose a category and let the shuffler pick
+            the perfect option for you and your partner!
+          </p>
+        </div>
+        <Menu />
+      </section>
     </>
   );
 }
