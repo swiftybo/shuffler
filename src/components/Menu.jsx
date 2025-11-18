d9e0e4f407191feeac8bdd4ca850f8f944e5e66a
swiftybo <leungjordan28@gmail.com>
2025-11-18 16:39:57 +0000

    Added categories prop to render the menuItem categories

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,16 +7,32 @@ import cookingLogo from "../assets/cooking-logo.png";
 export default function Menu() {
   return (
     <section className="menu">
-      <MenuItem title="Films" logo={filmLogo}>
-        Discover your next binge-watch or movie night pick.
+      <MenuItem
+        title="Films"
+        logo={filmLogo}
+        categories={["Netflix", "Cinema"]}
+      >
+        Discover your next binge-watch or movie-night pick.
       </MenuItem>
-      <MenuItem title="Restaurants" logo={restaurantLogo}>
+      <MenuItem
+        title="Restaurants"
+        logo={restaurantLogo}
+        categories={["Cuisine", "A la carte", "Buffet"]}
+      >
         Find the perfect spot to eat date night.
       </MenuItem>
-      <MenuItem title="Activities" logo={activityLogo}>
+      <MenuItem
+        title="Activities"
+        logo={activityLogo}
+        categories={["Indoor", "Outdoor", "Chill", "High-Adrenaline"]}
+      >
         Explore fun things to do together.
       </MenuItem>
-      <MenuItem title="Cook Together" logo={cookingLogo}>
+      <MenuItem
+        title="Cook Together"
+        logo={cookingLogo}
+        categories={["Quick", "One-Pot", "Gourmet"]}
+      >
         Cook something delicious together.
       </MenuItem>
     </section>
