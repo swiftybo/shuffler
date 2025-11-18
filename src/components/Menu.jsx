75daf4ff5df1364c6b74cedd5a4b48b79f3725dd
swiftybo <leungjordan28@gmail.com>
2025-11-18 16:24:55 +0000

    Logos added for each activity in the assets folder

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,14 +4,14 @@ export default function Menu() {
   return (
     <section className="menu">
       <MenuItem title="Films">
-        Discover your next binge-wathc or movie night pick
+        Discover your next binge-watch or movie night pick.
       </MenuItem>
       <MenuItem title="Restaurants">
-        Find the perfect spot to eat date night
+        Find the perfect spot to eat date night.
       </MenuItem>
-      <MenuItem title="Activities">Explore fun things to do together</MenuItem>
+      <MenuItem title="Activities">Explore fun things to do together.</MenuItem>
       <MenuItem title="Cook Together">
-        Cook something delicious together
+        Cook something delicious together.
       </MenuItem>
     </section>
   );
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,12 +2,10 @@ import { Fragment } from "react";
 
 export default function MenuItem({ title, children, ...props }) {
   return (
-    <div className="menuItem">
-      <button className="menuItem__btn">
-        <img src="../assets/film-icon.png" alt="logo" />
-        <h2>{title}</h2>
-        <p>{children}</p>
-      </button>
-    </div>
+    <button className="menuItem">
+      <img src="../assets/film-icon.png" alt="logo" />
+      <h2>{title}</h2>
+      <p className="menuItem__description">{children}</p>
+    </button>
   );
 }
