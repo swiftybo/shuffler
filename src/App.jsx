fbcd8c173beb7b85107b017074ce9becac015127
swiftybo <leungjordan28@gmail.com>
2025-11-20 12:24:08 +0000

    Added all code initially captured in App.jsx and moved to Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Menu from "./components/Menu.jsx";
 import Header from "./components/Header.jsx";
 import { useState } from "react";
 import { MenuContext } from "./store/menu-context.jsx";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/Home.jsx";
 
@@ -14,25 +14,15 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  // const [selectedCategory, setSelectedCategory] = useState("");
 
-  function onSelectCategory(event) {
-    setSelectedCategory(event.target.value);
-  }
+  // function onSelectCategory(event) {
+  //   setSelectedCategory(event.target.value);
+  // }
 
   return (
     <>
-      <Header />
-      <MenuContext className="content">
-        <div className="intro josefin-sans">
-          <h1>What should we do today?</h1>
-          <p className="intro__para">
-            Let the Shuffler decide! Choose a category and let the shuffler pick
-            the perfect option for you and your partner!
-          </p>
-        </div>
-        <Menu />
-      </MenuContext>
+      <RouterProvider router={router} />
     </>
   );
 }
