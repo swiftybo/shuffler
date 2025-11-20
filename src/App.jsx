d028774894a54c983d5f8ac1ba20c07310f299ca
swiftybo <leungjordan28@gmail.com>
2025-11-20 12:17:50 +0000

    Added in initial set up code for react router and added home page to pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import Menu from "./components/Menu.jsx";
 import Header from "./components/Header.jsx";
 import { useState } from "react";
 import { MenuContext } from "./store/menu-context.jsx";
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
