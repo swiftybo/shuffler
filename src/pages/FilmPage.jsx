f906b3e4e985affa2650e146f9d1249fd9d837ac
BoSloth <leungjordan28@gmail.com>
2025-12-03 12:09:38 +0000

    Changes made to styling files including new styling file for CategoryHeader

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,8 +1,10 @@
 import AvailableFilms from "../components/filmComponents/AvailableFilms";
+import CategoryHeader from "../components/generalComponents/categoryHeader";
 
 export default function FilmPage() {
   return (
     <>
+      <CategoryHeader />
       <AvailableFilms />
     </>
   );
