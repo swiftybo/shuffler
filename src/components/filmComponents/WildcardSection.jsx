6a6f319d08fc3a45b0112e79be95db3c85b312f5
BoSloth <leungjordan28@gmail.com>
2025-12-05 12:56:02 +0000

    new function added to httpRequests file to fetch the movieId of the movie that the user has entered in the wildcard movie section

--- src/components/filmComponents/WildcardSection.jsx
+++ src/components/filmComponents/WildcardSection.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react"
+import { fetchFilmID } from "../../httpRequests.js"
+
 export default function WildcardSection() {
+    const [userMovieTitle, setUserMovieTitle] = useState("")
+    const [userMovieYear, setUserMovieYear] = useState("")
+
     return (
         <>
             <h4>Give me an idea of the type of film you're feeling like...</h4>
-            <input />
-            <button>Go!</button>
+            <label name="movieTitle">Movie Title:</label>
+            <input value={userMovieTitle} onChange={(e) => setUserMovieTitle(e.target.value)}/>
+            <label name="movieYear">Year of Release:</label>
+            <input value={userMovieYear} onChange={(e) => setUserMovieYear(e.target.value)} />
+            <button onClick={() => fetchFilmID(userMovieTitle)}>Go!</button>
         </>
     )
 }
--- src/httpRequests.js
+++ src/httpRequests.js
@@ -16,3 +16,30 @@ export async function fetchFilms(movieList) {
     return availableMovies 
   
 }
+
+export async function fetchFilmID(movieTitle) {
+    
+//     const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTkwYzhlNmI2MTUzOGY2MzMwOTU1ZjY5Y2I2MmVkYSIsIm5iZiI6MTc2NDg0ODIzMC4zNjgsInN1YiI6IjY5MzE3MjY2NjI3ZDU4OTVmM2Y1YmE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cr23RiQKh_eUR3hG9Sa5xEDkCE2da6h4o0CN3Maqngg'
+//   }
+// };
+
+// fetch('https://api.themoviedb.org/3/search/movie?query=jaws&include_adult=false&language=en-US&page=1', options)
+//   .then(res => res.json())
+//   .then(res => console.log(res))
+    
+    const options = {
+        method: 'GET',
+        headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZTkwYzhlNmI2MTUzOGY2MzMwOTU1ZjY5Y2I2MmVkYSIsIm5iZiI6MTc2NDg0ODIzMC4zNjgsInN1YiI6IjY5MzE3MjY2NjI3ZDU4OTVmM2Y1YmE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cr23RiQKh_eUR3hG9Sa5xEDkCE2da6h4o0CN3Maqngg'
+        }
+    };
+
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movieTitle}&include_adult=false&language=en-US&page=1`, options)
+    const movie = await response.json()
+    console.log(movie)
+}
