af8414f592592164698cc005b99122abaf44b5ee
BoSloth <leungjordan28@gmail.com>
2025-12-05 17:34:55 +0000

    async await function created to find movies from user inputs using the tmdb api

--- src/components/filmComponents/WildcardSection.jsx
+++ src/components/filmComponents/WildcardSection.jsx
@@ -5,14 +5,21 @@ export default function WildcardSection() {
     const [userMovieTitle, setUserMovieTitle] = useState("")
     const [userMovieYear, setUserMovieYear] = useState("")
 
+    async function findUserMovie() {
+        const movies = await fetchFilmID(userMovieTitle, userMovieYear)
+        console.log(movies)
+    }
+
     return (
         <>
-            <h4>Give me an idea of the type of film you're feeling like...</h4>
-            <label name="movieTitle">Movie Title:</label>
-            <input value={userMovieTitle} onChange={(e) => setUserMovieTitle(e.target.value)}/>
-            <label name="movieYear">Year of Release:</label>
-            <input value={userMovieYear} onChange={(e) => setUserMovieYear(e.target.value)} />
-            <button onClick={() => fetchFilmID(userMovieTitle)}>Go!</button>
+            <div>
+                <h4>Give me an idea of the type of film you're feeling like...</h4>
+                <label name="movieTitle">Movie Title:</label>
+                <input value={userMovieTitle} onChange={(e) => setUserMovieTitle(e.target.value)}/>
+                <label name="movieYear">Year of Release:</label>
+                <input value={userMovieYear} onChange={(e) => setUserMovieYear(e.target.value)} />
+                <button onClick={findUserMovie}>Go!</button>
+            </div>
         </>
     )
 }
