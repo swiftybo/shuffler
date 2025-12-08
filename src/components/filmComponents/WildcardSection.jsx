a430550c28f7c62011ee570c88e5428e99e6b14e
BoSloth <leungjordan28@gmail.com>
2025-12-08 09:35:15 +0000

    style module added for WildcardSection and buttons added to navigate through movies

--- src/components/filmComponents/WildcardSection.jsx
+++ src/components/filmComponents/WildcardSection.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+import { useState, useReducer } from "react"
 import { fetchFilmID } from "../../httpRequests.js"
+import classes from "./WildCardSection.module.css"
 import FilmItem from "./FilmItem.jsx"
 
 export default function WildcardSection() {
@@ -7,10 +8,12 @@ export default function WildcardSection() {
     const [userMovieYear, setUserMovieYear] = useState("")
     const [identifiedMovies, setIdentifiedMovies] = useState([])
     const [movieIndex, setMovieIndex] = useState(0)
+    const [confirmedMovieID, setConfirmedMovieID] = useState()
 
     async function findUserMovie() {
         const {results} = await fetchFilmID(userMovieTitle, userMovieYear)
         setIdentifiedMovies([...results])
+        setMovieIndex(0)
         console.log(results)
     }
 
@@ -24,13 +27,19 @@ export default function WildcardSection() {
                 <input value={userMovieYear} onChange={(e) => setUserMovieYear(e.target.value)} />
                 <button onClick={findUserMovie}>Go!</button>
             </div>
-            <section>
-                {identifiedMovies.length > 0 && 
-                    <div>
+            {identifiedMovies.length > 0 && <section>
+                <div className={classes.wildcardMovie}>
+                    <img src={`https://image.tmdb.org/t/p/w185${identifiedMovies[movieIndex].poster_path}`} />
+                    <div className={classes.wildcardMovie__summary}>
                         <h2>{identifiedMovies[movieIndex].original_title}</h2>
-                        <img src={`https://image.tmdb.org/t/p/w185${identifiedMovies[movieIndex].poster_path}`} />
-                    </div>}
-            </section>
+                        <p>Summary: {identifiedMovies[movieIndex].overview}</p>
+                        <p>Release Date: {identifiedMovies[movieIndex].release_date}</p>
+                    </div>
+                </div>
+                {movieIndex > 0 && <button className={classes.wildcardMovie__rejectBtn} onClick={() => setMovieIndex(prevValue => prevValue - 1)}>Previous Movie</button>}
+                <button className={classes.wildcardMovie__confirmBtn} onClick={() => setConfirmedMovieID(identifiedMovies[movieIndex].id)}>Confirm</button>
+                {movieIndex < identifiedMovies.length -1 && <button className={classes.wildcardMovie__rejectBtn} onClick={() => setMovieIndex(prevValue => prevValue + 1)}>Next Movie</button>}
+            </section>}
         </>
     )
 }
