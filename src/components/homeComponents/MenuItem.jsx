823398d7b2a8050c3c8f8be6cd0468184ad2c216
BoSloth <leungjordan28@gmail.com>
2025-12-09 15:16:10 +0000

    removed menuCategory component and fine tuned all the styling for the homepage components and the navbar components

--- src/components/homeComponents/MenuItem.jsx
+++ src/components/homeComponents/MenuItem.jsx
@@ -1,14 +1,12 @@
-import MenuCategory from "./MenuCategory.jsx";
-
 export default function MenuItem({ title, logo, buzzWords, children }) {
   return (
     <div className="menuItem">
-      <img className="menuItem__logo" src={logo} alt="logo" />
-      <h3>{title}</h3>
+      <img className="menuItem__logo" src={logo} alt={`${title} logo`} />
+      <h3 className="menuItem__header">{title}</h3>
       <p className="menuItem__description josefin-sans">{children}</p>
-      <section className="menuItem__buzzWords">
+      <section className="menuItem__buzzSection">
         {buzzWords.map((buzzWord) => (
-          <MenuCategory buzzWord={buzzWord} />
+          <div className="menuItem__buzzword">{buzzWord}</div>
         ))}
       </section>
     </div>
