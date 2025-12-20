01f73514a7d6039bf2ba38621c8a81764e126add
BoSloth <leungjordan28@gmail.com>
2025-12-20 18:55:28 +0000

    number of films is now calculated in categoryHeader to filter out undefined entries to the AvailableFilms state. This allows for more accurate total number of films

--- src/components/generalComponents/categoryHeader.jsx
+++ src/components/generalComponents/categoryHeader.jsx
@@ -5,13 +5,16 @@ import { useFilmContext } from "../../store/film-context"
 export default function CategoryHeader() {
     const {fetchedFilms, watchedFilms, unwatchedFilms, isFetching} = useFilmContext()
 
+    // In scenario where film cannot be fetched, we do not want this unidentified movie to be part of the total film count
+    const filmCount = fetchedFilms.filter(film => film !== undefined).length
+
     return (
         <header className={classes.header}>
             <img className={classes.header__logo} src={filmLogo} alt="film logo"></img>
             <h2 className={classes.header__title}>Films & Shows</h2>
             {isFetching && <p style={{fontSize: "1.1rem", color: "rgba(78, 78, 78)"}}>Fetching films...</p>}
             {!isFetching && <>
-                <p style={{fontSize: "1.1rem", color: "rgba(78, 78, 78)"}}>{fetchedFilms.length} films in your list</p>
+                <p style={{fontSize: "1.1rem", color: "rgba(78, 78, 78)"}}>{filmCount} films in your list</p>
                 <div className={classes.header__statsSection}>
                     <p className={classes.header__stat}><span style={{color: "green"}}>✔ </span>{watchedFilms.length} watched</p>
                     <p className={classes.header__stat}><span>👀</span>{unwatchedFilms.length} to watch</p>
