fcf46eab7c240bc1b3521ea5166d5b17990217aa
BoSloth <leungjordan28@gmail.com>
2025-12-21 19:10:11 +0000

    added checkbox to FilmHeader component which decides if randomizer uses filtered or all movies list. This is controlled by state in the film context

--- src/components/filmComponents/RandomizerSection.jsx
+++ src/components/filmComponents/RandomizerSection.jsx
@@ -5,14 +5,19 @@ import rerollIcon from "../../assets/reroll-icon.png"
 import { useFilmContext } from "../../store/film-context"
 
 export default function RandomizerSection() {
-    const {fetchedFilms} = useFilmContext()
+    const {isRandomizingFilteredFilms, fetchedFilms, unwatchedFilms} = useFilmContext()
 
     const [isRandomizing, setIsRandomizing] = useState(true)
     // Saving the randomized movie in this new state causes the "mark as watched" button to not work as this is not connected to the fetchedFilms state. 
     const [randomizedMovieIndex, setRandomizedMovieIndex] = useState()
 
     function fetchRandomMovie() {
-        const randomisedMovie = fetchedFilms[Math.floor(Math.random() * fetchedFilms.length)]
+        let randomisedMovie
+        if (isRandomizingFilteredFilms) {
+            randomisedMovie = unwatchedFilms[Math.floor(Math.random() * unwatchedFilms.length)]
+        } else {
+            randomisedMovie = fetchedFilms[Math.floor(Math.random() * fetchedFilms.length)]
+        }
         setRandomizedMovieIndex(fetchedFilms.findIndex(film => film === randomisedMovie))
     }
 
@@ -32,7 +37,6 @@ export default function RandomizerSection() {
     return (
         <div className={classes.randomizerSection}>
             <p className={`${classes.randomizerSection__para}`}>Your randomized pick:</p>
-            {/* TODO: Needs updating to reflect new state name */}
             {randomizedMovieIndex >= 0 && <FilmItem selectedFilm={fetchedFilms[randomizedMovieIndex]}/>}
             <button className={classes.randomizerSection__button} onClick={() => setIsRandomizing(true)} disabled={isRandomizing}>
                 <img className={classes.randomizerSection__buttonIcon}src={rerollIcon} alt="reroll button icon" />
